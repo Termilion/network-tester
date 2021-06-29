0947ab05bbf350d0be51a5abc368f614dbed3097
Lucas Ehrich <lucas.ehrich@googlemail.com>
2021-06-29 10:56:09 +0200

    Fix batch files

--- startIoT.bat
+++ startIoT.bat
@@ -1,2 +1,2 @@
 git pull
-java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client -u -iot -r 1500 -d %1 -n 192.168.1.9 192.168.1.9 5000
+java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client -u -iot -r 1500 -d %1 -ntp 192.168.1.9 192.168.1.9 5000
--- startServer.bat
+++ startServer.bat
@@ -1,2 +1,2 @@
 git pull
-java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Server -n localhost
+java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Server -ntp localhost
