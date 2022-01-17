70b35feae0cb96f480fd1df23a81f1c7b582e402
Lucas Ehrich <ehrich@rvs.informatik.uni-leipzig.de>
2022-01-17 09:55:31 +0100

    Updates *.bat files with new parameters

--- startIoT.bat
+++ startIoT.bat
@@ -1,2 +1,2 @@
 git pull
-java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client --ntp 192.168.1.9:9999 -r 1500 -d %1 192.168.1.9 10000 --trace 25 IOT UP
+java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client --data-ip=192.168.1.9 --ntp=192.168.1.9:9999 -r=1500 -d=%1 --trace=25 192.168.2.9 10000 IOT UP
--- startServer.bat
+++ startServer.bat
@@ -1,2 +1,2 @@
 git pull
-java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Server --ntpServerPort 9999 --trace 25 10000 7
+java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Server --ntpServerPort=9999 --trace=25 10000 7
