837e59a5bed6ed98207f4c924edb993740ff544e
MadTracki <lucas.ehrich@googlemail.com>
2022-01-17 10:53:04 +0100

    Updates *.bat files

--- startBulk.bat
+++ startBulk.bat
@@ -1,2 +1,2 @@
 git pull
-java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client --data-ip=192.168.1.9 --ntp=192.168.1.9:9999 --trace=25 -d=%1 192.168.2.9 10000 BULK DOWN
+java -jar .\target\NetworkTester-1.0-SNAPSHOT-jar-with-dependencies.jar Client --data-ip=192.168.1.9 --ntp=192.168.2.9:9999 --trace=25 -d=%1 192.168.2.9 10000 BULK DOWN
